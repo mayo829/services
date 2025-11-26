7fc73c0e6f23ddf5b95e09218e48b8b7e9aabed9
mayo829 <mayo@yo>
2025-11-26 01:32:44 -0500

    u

--- application/routes.go
+++ application/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
-	"github.com/mayo829/services/handler"
+	// "github.com/mayo829/services/handler"
 )
 
 func loadRoutes() *chi.Mux {
