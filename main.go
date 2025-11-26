66d9482118db162fe3546368a721a6c6f4244d2f
mayo829 <mayo@yo>
2025-11-26 00:59:23 -0500

    init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewRouter()
+
+	router.Get("/hello", basicHandler)
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to listen to server", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world"))
+}
